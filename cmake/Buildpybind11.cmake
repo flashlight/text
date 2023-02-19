8e939de76b330e66b50d6e73b2292fb71c5b23b8
jacobkahn <jacobkahn1@gmail.com>
2023-02-18 16:46:44 -0800

    Try manual find package

--- cmake/Buildpybind11.cmake
+++ cmake/Buildpybind11.cmake
@@ -13,18 +13,3 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(pybind11)
 add_subdirectory(${pybind11_SOURCE_DIR})
-
-# # Download pybind11
-# ExternalProject_Add(
-#   pybind11
-#   PREFIX pybind11
-#   GIT_REPOSITORY ${pybind11_URL}
-#   GIT_TAG ${pybind11_TAG}
-#   BUILD_IN_SOURCE 0
-#   CONFIGURE_COMMAND ""
-#   BUILD_COMMAND ""
-#   INSTALL_COMMAND ""
-# )
-
-# ExternalProject_Get_Property(pybind11 SOURCE_DIR)
-# set(pybind11_INCLUDE_DIR "${SOURCE_DIR}/include")
