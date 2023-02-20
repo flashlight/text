1cbe1cb932c5117a090f66899f8313bdde338a8a
jacobkahn <jacobkahn1@gmail.com>
2023-02-19 19:52:40 -0800

    Is Python3_EXECUTABLE case sensitive?

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ class CMakeBuild(build_ext):
         )
         cmake_args = [
             "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + ext_dir,
-            "-DPYTHON3_EXECUTABLE=" + sys.executable,
+            "-DPython3_EXECUTABLE=" + sys.executable,
             "-DBUILD_SHARED_LIBS=ON",
             "-DFL_TEXT_BUILD_STANDALONE=OFF",
             "-DFL_TEXT_BUILD_TESTS=OFF",
