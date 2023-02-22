ebab3336632ff10d0639690b4f4b5dabaad443d6
jacobkahn <jacobkahn1@gmail.com>
2023-02-22 11:49:04 -0800

    Swap to CMake FetchContent for KenLM, remove kenlm_utils dependency (#51)
    Summary:
    See title. Now that we're not using `ExternalProject`, KenLM installs properly with Flashlight Text, even when Flashlight Text is pulled in via CMake `FetchContent` (e.g. in Flashlight). This also removes the dependence on the kenlm_util library, and since no Boost symbols are required here, this removes the Boost requirement for Flashlight Text + KenLM setups, as KenLM can be built with `BUILD_TOOLS=OFF`, which removes the Boost dep.
    
    One annoying consequence is that the include path of KenLM when pulled in with FetchContent is tough to wrangle to a custom apex dir, so `lm/...` is now the entry point header for KenLM components and their internal includes. This should still work in fbcode given existing setups/using langtech. Some build systems using extremely old versions of KenLM will no longer work, but those envs should update.
    
    Pull Request resolved: https://github.com/flashlight/text/pull/51
    
    Test Plan: CI + cibw + numerous local tests.
    
    Reviewed By: richjames0
    
    Differential Revision: D43480391
    
    Pulled By: jacobkahn
    
    fbshipit-source-id: 4789dc5fcb5ed6f287b565fbae6ea32656f90a24

--- cmake/BuildKenlm.cmake
+++ cmake/BuildKenlm.cmake
@@ -1,71 +1,31 @@
-cmake_minimum_required(VERSION 3.10.0)
+cmake_minimum_required(VERSION 3.16)
 
-# Required for KenLM to read ARPA files in compressed format
-find_package(LibLZMA REQUIRED)
-find_package(BZip2 REQUIRED)
-find_package(ZLIB REQUIRED)
+include(FetchContent)
 
-set(COMPRESSION_LIBS
-  ${LIBLZMA_LIBRARIES}
-  ${BZIP2_LIBRARIES}
-  ${ZLIB_LIBRARIES}
-  )
+# TODO: swap to main repo once https://github.com/kpu/kenlm/pull/418 is merged
+set(kenlm_URL https://github.com/jacobkahn/kenlm.git)
+set(kenlm_TAG 9c05ca013a793826ba0ff1945ae6b9f16ca859a5)
 
-include(ExternalProject)
-
-set(kenlm_TEMP_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/extern/kenlm)
-set(kenlm_URL https://github.com/kpu/kenlm.git)
-set(kenlm_BUILD ${CMAKE_CURRENT_BINARY_DIR}/third-party/kenlm)
-set(kenlm_TAG 5cea457db26950a73d638425c183b368c06ed7c6)
-set(kenlm_BINARY_DIR ${kenlm_BUILD}/src/kenlm-build)
-
-if (BUILD_SHARED_LIBS)
-  set(LIB_TYPE SHARED)
-else()
-  set(LIB_TYPE STATIC)
-endif()
-
-set(KENLM_LIB_PATH ${kenlm_BINARY_DIR}/lib/${CMAKE_${LIB_TYPE}_LIBRARY_PREFIX}kenlm${CMAKE_${LIB_TYPE}_LIBRARY_SUFFIX})
-set(KENLM_UTIL_LIB_PATH ${kenlm_BINARY_DIR}/lib/${CMAKE_${LIB_TYPE}_LIBRARY_PREFIX}kenlm_util${CMAKE_${LIB_TYPE}_LIBRARY_SUFFIX})
-set(KENLM_BUILT_LIBRARIES ${KENLM_LIB_PATH} ${KENLM_UTIL_LIB_PATH})
-
-if (NOT TARGET kenlm)
-  # Download kenlm
-  ExternalProject_Add(
+FetchContent_Declare(
     kenlm
-    PREFIX ${kenlm_BUILD}
     GIT_REPOSITORY ${kenlm_URL}
     GIT_TAG ${kenlm_TAG}
-    BUILD_BYPRODUCTS
-      ${KENLM_BUILT_LIBRARIES}
-    CMAKE_CACHE_ARGS
-      -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
-      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-      -DCMAKE_INSTALL_PREFIX:PATH=${kenlm_TEMP_INSTALL_DIR}
-      )
-endif ()
+)
 
-# Install the install executed at build time
-install(DIRECTORY ${kenlm_TEMP_INSTALL_DIR}/include DESTINATION ${CMAKE_INSTALL_PREFIX})
-install(DIRECTORY ${kenlm_TEMP_INSTALL_DIR}/lib DESTINATION ${CMAKE_INSTALL_PREFIX})
-install(DIRECTORY ${kenlm_TEMP_INSTALL_DIR}/bin DESTINATION ${CMAKE_INSTALL_PREFIX})
+set(_BUILD_TESTING ${BUILD_TESTING}) # save if already set
+# KenLM build options
+set(ENABLE_INTERPOLATE OFF CACHE BOOL "" FORCE)
+set(COMPILE_TESTS OFF CACHE BOOL "" FORCE)
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+set(BUILD_PYTHON OFF CACHE BOOL "" FORCE)
+set(FORCE_STATIC OFF CACHE BOOL "" FORCE)
+set(BUILD_TOOLS OFF CACHE BOOL "" FORCE)
+set(BUILD_BENCHMARKS OFF CACHE BOOL "" FORCE)
 
-set(KENLM_INCLUDE_DIRS "${kenlm_TEMP_INSTALL_DIR}/include;${kenlm_TEMP_INSTALL_DIR}/include/kenlm")
-file(MAKE_DIRECTORY ${KENLM_INCLUDE_DIRS})
+FetchContent_MakeAvailable(kenlm)
 
-set(KENLM_LIBRARIES ${KENLM_BUILT_LIBRARIES} ${COMPRESSION_LIBS})
+# includes are relative to the source dir
+target_include_directories(kenlm PUBLIC $<BUILD_INTERFACE:${kenlm_SOURCE_DIR}>)
+add_library(kenlm::kenlm ALIAS kenlm)
 
-if (NOT TARGET kenlm::kenlm)
-  add_library(kenlm::kenlm ${LIB_TYPE} IMPORTED)
-  set_property(TARGET kenlm::kenlm PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${KENLM_INCLUDE_DIRS})
-  set_property(TARGET kenlm::kenlm PROPERTY IMPORTED_LOCATION ${KENLM_LIB_PATH})
-  add_dependencies(kenlm::kenlm kenlm)
-endif()
-
-if (NOT TARGET kenlm::kenlm_util)
-  add_library(kenlm::kenlm_util ${LIB_TYPE} IMPORTED)
-  set_property(TARGET kenlm::kenlm_util PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${KENLM_INCLUDE_DIRS})
-  set_property(TARGET kenlm::kenlm_util PROPERTY IMPORTED_LOCATION ${KENLM_UTIL_LIB_PATH})
-  set_property(TARGET kenlm::kenlm_util PROPERTY IMPORTED_LINK_INTERFACE_LIBRARIES ${COMPRESSION_LIBS})
-  add_dependencies(kenlm::kenlm_util kenlm)
-endif()
+set(BUILD_TESTING ${_BUILD_TESTING}) # restore
--- cmake/Findkenlm.cmake
+++ cmake/Findkenlm.cmake
@@ -1,8 +1,5 @@
 # Try to find the KenLM library
 #
-# Options:
-#   KENLM_DISABLE_UTILS_SEARCH: disable searching for libkenlm_utils
-#
 # The following variables are optionally searched for defaults
 #   KENLM_ROOT: Base directory where all KENLM components are found
 #
@@ -13,13 +10,6 @@
 #
 
 message(STATUS "Looking for KenLM.")
-
-if (NOT KENLM_DISABLE_UTILS_SEARCH)
-  # Required for KenLM to read ARPA files in compressed format
-  find_package(LibLZMA REQUIRED)
-  find_package(BZip2 REQUIRED)
-  find_package(ZLIB REQUIRED)
-endif()
 
 find_library(
   KENLM_LIB
@@ -56,55 +46,10 @@ if (WIN32)
     )
 endif()
 
-if (NOT KENLM_DISABLE_UTILS_SEARCH)
-  find_library(
-    KENLM_UTIL_LIB
-    kenlm_util
-    HINTS
-    ${KENLM_UTIL_LIB_PATH}
-    ${KENLM_ROOT}
-    ${KENLM_ROOT}/lib
-    ${KENLM_ROOT}/build/lib
-    PATHS
-    $ENV{KENLM_UTIL_LIB_PATH}
-    $ENV{KENLM_ROOT}
-    $ENV{KENLM_ROOT}/lib
-    $ENV{KENLM_ROOT}/build/lib
-    )
-
-  if (WIN32)
-    find_library(
-      KENLM_UTIL_IMPLIB
-      NAMES
-      kenlm_util.lib
-      HINTS
-      ${KENLM_LIB_PATH}
-      ${KENLM_ROOT}
-      ${KENLM_ROOT}/lib
-      ${KENLM_ROOT}/build/lib
-      PATHS
-      $ENV{KENLM_LIB_PATH}
-      $ENV{KENLM_ROOT}
-      $ENV{KENLM_ROOT}/lib
-      $ENV{KENLM_ROOT}/build/lib
-      )
-  endif()
-endif()
-
 if(KENLM_LIB)
   message(STATUS "Using kenlm library found in ${KENLM_LIB}")
 else()
   message(STATUS "kenlm library not found; if you already have kenlm installed, please set CMAKE_LIBRARY_PATH, KENLM_LIB or KENLM_ROOT environment variable")
-endif()
-
-if(KENLM_UTIL_LIB)
-  message(STATUS "Using kenlm utils library found in ${KENLM_UTIL_LIB}")
-else()
-  if (NOT KENLM_DISABLE_UTILS_SEARCH)
-    message(STATUS "kenlm utils library not found; if you already have kenlm installed, please set CMAKE_LIBRARY_PATH, KENLM_UTIL_LIB or KENLM_ROOT environment variable")
-  else()
-    message(STATUS "Findkenlm: KENLM_DISABLE_UTILS_SEARCH enabled - not searching for kenlm utils. Will not link when building")
-  endif()
 endif()
 
 # find a model header, then get the entire include directory. We need to do this because
@@ -145,10 +90,6 @@ set(KENLM_LIBRARIES
   ${KENLM_LIB}
   ${COMPRESSION_LIBRARIES}
   )
-if (NOT KENLM_DISABLE_UTILS_SEARCH)
-  # only need utils if finding it is not disabled
-  list(APPEND KENLM_LIBRARIES ${KENLM_UTIL_LIB})
-endif()
 
 # Some KenLM include paths are relative to [include dir]/kenlm, not just [include dir] (bad)
 set(KENLM_INCLUDE_DIRS "${KENLM_INCLUDE_DIR};${KENLM_INCLUDE_LM}")
@@ -171,13 +112,5 @@ if (kenlm_FOUND)
     set_property(TARGET kenlm::kenlm PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${KENLM_INCLUDE_DIRS})
     set_property(TARGET kenlm::kenlm PROPERTY IMPORTED_LOCATION ${KENLM_LIB})
     set_property(TARGET kenlm::kenlm PROPERTY IMPORTED_IMPLIB ${KENLM_IMPLIB})
-  endif()
-
-  if (NOT TARGET kenlm::kenlm_util AND NOT KENLM_DISABLE_UTILS_SEARCH)
-    add_library(kenlm::kenlm_util ${LIB_TYPE} IMPORTED)
-    set_property(TARGET kenlm::kenlm_util PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${KENLM_INCLUDE_DIRS})
-    set_property(TARGET kenlm::kenlm_util PROPERTY IMPORTED_LOCATION ${KENLM_UTIL_LIB})
-    set_property(TARGET kenlm::kenlm_util PROPERTY IMPORTED_IMPLIB ${KENLM_UTIL_IMPLIB})
-    set_property(TARGET kenlm::kenlm_util PROPERTY IMPORTED_LINK_INTERFACE_LIBRARIES ${COMPRESSION_LIBS})
   endif()
 endif()
