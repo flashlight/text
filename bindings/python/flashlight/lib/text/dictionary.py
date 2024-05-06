8936714f6717641347f04e23eda03f3aa5d37eef
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-05-06 16:14:11 -0700

    deeplearning/projects/flashlight-text/bindings/python:lib_text_dictionary
    Reviewed By: AishwaryaSivaraman
    
    Differential Revision: D57014310
    
    fbshipit-source-id: 033bec9b537823ee50fb6be7a60aa07ab0c57154

--- bindings/python/flashlight/lib/text/dictionary.py
+++ bindings/python/flashlight/lib/text/dictionary.py
@@ -6,7 +6,7 @@ This source code is licensed under the MIT-style license found in the
 LICENSE file in the root directory of this source tree.
 """
 
-from .flashlight_lib_text_dictionary import (
+from .flashlight_lib_text_dictionary import (  # @manual=fbcode//deeplearning/projects/flashlight-text/bindings/python:flashlight_lib_text_dictionary
     create_word_dict,
     Dictionary,
     load_words,
